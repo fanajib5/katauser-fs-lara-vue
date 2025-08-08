0e771e571747ebadbe88babafbd516d8089b6b90
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2025-08-08 14:58:14 +0700

    feat: Initial plan model

---
+++ database/migrations/2025_08_08_072647_create_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->string('description')->nullable();
+            $table->unsignedInteger('version')->default(1);
+            $table->boolean('status');
+            $table->foreignId('created_by')->constrained('users')->restrictOnDelete();
+            $table->foreignId('updated_by')->constrained('users')->restrictOnDelete();
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->restrictOnDelete();
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
