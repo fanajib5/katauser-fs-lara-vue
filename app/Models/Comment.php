57a75978674119885e3cf3380aef05e48203f867
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2025-08-11 13:19:58 +0700

    feat: Add comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'feedback_post_id',
+        'member_id',
+        'parent_id',
+        'content',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
---
+++ database/migrations/2025_08_11_061508_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('feedback_post_id')->constrained('feedback_posts')->restrictOnDelete();
+            $table->foreignId('member_id')->constrained('members')->restrictOnDelete();
+            $table->text('content');
+            $table->foreignId('parent_id')->nullable()->constrained('comments')->restrictOnDelete();
+            $table->foreignId('created_by')->constrained('users')->restrictOnDelete();
+            $table->foreignId('updated_by')->constrained('users')->restrictOnDelete();
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->restrictOnDelete();
+            $table->timestampsTz();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
