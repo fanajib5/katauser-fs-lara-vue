aa64beb92a440a6730afce0660acdd15072a454a
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2025-08-13 09:42:03 +0700

    fix: Change comments into english

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum UserRole: string
 {
-    case DEVELOPER = 'developer'; // role yang memiliki hak akses penuh pada platform
-    case ADMIN = 'admin'; // role yang memiliki hak akses penuh pada organisasi
-    case MEMBER = 'member'; // role yang sudah terdaftar dalam organisasi
-    case GUEST = 'guest'; // role yang belum daftar
-    case USER = 'user'; // role yang tidak termasuk dalam organisasi manapun
+    case DEVELOPER = 'developer'; // role that has full access rights on the platform
+    case ADMIN = 'admin'; // role that has full access rights on the organization
+    case MEMBER = 'member'; // role that is registered within the organization
+    case GUEST = 'guest'; // role that is not registered yet
+    case USER = 'user'; // role that does not belong to any organization
 }
