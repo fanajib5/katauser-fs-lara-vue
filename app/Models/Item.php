9ce350a5488ac87500b6ca01dee77da98d057a66
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2025-08-13 09:54:52 +0700

    fix: Add tracks changes trait

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\ItemType;
+use App\Traits\TracksChanges;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, TracksChanges, SoftDeletes;
 
     protected $fillable = [
         'plan_id',
